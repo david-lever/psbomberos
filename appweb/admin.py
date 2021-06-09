e02340f538a985099c5437fe1febcb728641074e
david-lever <da.lever@duocuc.cl>
2021-06-08 22:05:27 -0400

    Update admin.py

--- appweb/admin.py
+++ appweb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Personal, Cargo, Compania, Comuna, Provincia, Region  
 
 # Register your models here.
+
+
+admin.site.register(Personal)
+admin.site.register(Cargo)
+admin.site.register(Compania)
+admin.site.register(Comuna)
+admin.site.register(Provincia)
+admin.site.register(Region)
